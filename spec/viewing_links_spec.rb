9939aec9b4240dc1cc6fc694720c2c4802380256
John Chang <johnchang9094@gmail.com>
2017-03-07 12:34:55 +0000

    Completed exercise 10, got the feature test passing. expect(page.status_code).to eq 200. Furthermore, created views folder and index.erb

--- spec/viewing_links_spec.rb
+++ spec/viewing_links_spec.rb
@@ -1,12 +1,15 @@
+require './app/models/link.rb'
+
 feature 'Viewing links' do
   scenario 'so that I can see existing links on the webpage' do
-    Link.create(url: 'https://google.co.uk', title: 'Google')
+    Link.create(url: 'http://www.facebook.com', title: 'Facebook')
 
     visit '/links'
+
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-      expect(page).to have_content 'Google'
+      expect(page).to have_content('Facebook')
     end
   end
 end
