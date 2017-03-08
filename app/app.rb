58f245caf93f02db7312e473f57446587d4936a8
John Chang <johnchang9094@gmail.com>
2017-03-08 15:02:12 +0000

    Implementing Heroku

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,8 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require './app/models/link.rb'
 require 'database_cleaner'
-require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
