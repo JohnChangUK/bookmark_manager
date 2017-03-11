e9266f8fc2332490cf96dfdd593f3a5bde7ada96
John Chang <johnchang9094@gmail.com>
2017-03-11 12:53:00 +0000

    refactoring

--- app/app.rb
+++ app/app.rb
@@ -23,17 +23,17 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     link = Link.create(url: params[:url], title: params[:title])
     params[:tags].split.each do |tag|
-      link.tags << Tag.first_or_create(name: tag)
+    link.tags << Tag.first_or_create(name: tag)
     end
     link.save
     redirect '/links'
   end
 
   get '/tags/:name' do
-  tag = Tag.first(name: params[:name])
-  @links = tag ? tag.links : []
-  erb :'links/index'
-end
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
 
   get '/users/new' do
     erb :'users/new'
@@ -49,7 +49,7 @@ end
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
-    end 
+    end
   end
 
 # start the server if ruby file executed directly
