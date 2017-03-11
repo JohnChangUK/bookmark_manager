b52026e02bd1bff3914e9d3730976ce728f61096
John Chang <johnchang9094@gmail.com>
2017-03-11 12:48:24 +0000

    Implementing BCrypt

--- app/app.rb
+++ app/app.rb
@@ -9,6 +9,7 @@ require_relative 'data_mapper_setup'
 class BookmarkManager < Sinatra::Base
 
   enable :sessions
+  set :session_secret, 'super_secret'
 
   get '/links' do
     @links = Link.all
@@ -20,7 +21,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url], title: params[:title])
+    link = Link.create(url: params[:url], title: params[:title])
     params[:tags].split.each do |tag|
       link.tags << Tag.first_or_create(name: tag)
     end
@@ -34,11 +35,22 @@ class BookmarkManager < Sinatra::Base
   erb :'links/index'
 end
 
-post '/tags/:name' do
-tag = Tag.first(name: params[:name])
-@links = tag ? tag.links : []
-erb :'links/index'
-end
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+    password: params[:password])
+    session[:user_id] = user.id
+    redirect '/links'
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end 
+  end
 
 # start the server if ruby file executed directly
   run! if app_file == $0
