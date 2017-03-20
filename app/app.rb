6e0ab00e4c8bd2c6917679e25797b76e920bbc0c
John Chang <johnchang9094@gmail.com>
2017-03-20 23:23:55 +0000

    Creating route for POST sessions route

--- app/app.rb
+++ app/app.rb
@@ -54,12 +54,27 @@ class BookmarkManager < Sinatra::Base
                        password_confirmation: params[:password_confirmation])
     if @user.save
     session[:user_id] = @user.id
-    redirect to('/links')
+    redirect to('/')
   else 
-    flash.now[:notice] = 'Password and confirmation password do not match'  
+    flash.now[:errors] = @user.errors.full_messages
     erb :'users/new'
   end
 end 
+
+  get '/sessions/new' do 
+    erb :'sessions/new'
+    end 
+
+    post '/sessions' do 
+      user = User.authenticate(params[:email], params[:password])
+      if user 
+        session[:user_id] = user_id
+        redirect to('/links')
+      else 
+        flash.now[:errors] = ['The email or password is incorrect']
+        erb :'sessions/new'
+      end 
+    end 
 
   helpers do
     def current_user
