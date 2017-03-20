c9ef482c7d78d18371a984547929e5ba0e37bc0f
John Chang <johnchang9094@gmail.com>
2017-03-20 22:36:02 +0000

    Slight Refactoring

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,6 +1,16 @@
 require 'web_helper'
 require './app/models/user'
 
+  def sign_up(email: 'john@makers.com',
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
+
 feature 'User sign up' do
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
@@ -18,15 +28,7 @@ feature 'User sign up' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
   end
 
-  def sign_up(email: 'alice@example.com',
-              password: '12345678',
-              password_confirmation: '12345678')
-    visit '/users/new'
-    fill_in :email, with: email
-    fill_in :password, with: password
-    fill_in :password_confirmation, with: password_confirmation
-    click_button 'Sign up'
-  end
+
 
   scenario 'with a password that does not match' do 
     expect {sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
